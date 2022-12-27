9161e3b3589117cf762d026cd98f9aa92fdb63c0
kalekisan <121477445+kalekisan@users.noreply.github.com>
2022-12-27 06:35:19 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,42 @@
 #include<iostream>
 using namespace std;
 
-int add(int p,int q)
+
+class Add
 {
-   return p+q;
+    private:
+    int i,j;
+    public:
+
+    Add()
+    {
+        cout<<"In default Condtructor"<<endl;
+    }
+    ~Add()
+    {
+        cout<<"Mem Is deallocated Successfully"<<endl;
+    }
+
+    int add(int p,int q)
+{
+    i=p;
+    j=q;
+   return i+j;
 }
 
-main()
-{
-    int i=10,j=20,result;
-    cout<<"Value of i :" <<i<<" and j is :"<<j<<endl;
 
-    result=add(i,j);
+};
+
+
+
+int main()
+{
+
+    int i=10,j=20,result;
+    Add obj;
+    //cout<<"Value of i :" <<i<<" and j is :"<<j<<endl;
+
+    result=obj.add(i,j);
 
     cout<<"Add is :"<<result<<endl;
 
