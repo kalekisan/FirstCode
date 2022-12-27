77ba57d18855868ae9e5fcd4493d54d8ec4ca478
kalekisan <121477445+kalekisan@users.noreply.github.com>
2022-12-27 06:24:37 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 
+int add(int p,int q)
+{
+   return p+q;
+}
+
 main()
 {
-    int i=10,j=20;
+    int i=10,j=20,result;
     cout<<"Value of i :" <<i<<" and j is :"<<j<<endl;
+
+    result=add(i,j);
+
+    cout<<"Add is :"<<result<<endl;
 
 }
